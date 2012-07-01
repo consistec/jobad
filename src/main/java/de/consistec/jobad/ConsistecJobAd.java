aa8633644306b7831228e5c0160d285cb590aebd
Christophe Boutter <boutta@gmail.com>
2012-07-01 18:21:55 +0200

    More ideas.

--- src/main/java/de/consistec/jobad/ConsistecJobAd.java
+++ src/main/java/de/consistec/jobad/ConsistecJobAd.java
@@ -6,6 +6,7 @@ public class ConsistecJobAd {
 	public static JavaDeveloper findNewColleague() {
 		// Hibernate query with params to find a dev, filter further...
 		// HasFun.workingWith(Beans.flavored(Jelly().or(Enterprise())));
+		// groovy rubies...
 		
 		return null;
 	}
