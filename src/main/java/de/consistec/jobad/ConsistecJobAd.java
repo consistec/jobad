08ede57d799a9fd37488aaa716f7da4df011c774
Christophe Boutter <boutta@gmail.com>
2012-07-01 18:04:06 +0200

    Initial commit

---
+++ src/main/java/de/consistec/jobad/ConsistecJobAd.java
@@ -0,0 +1,12 @@
+package de.consistec.jobad;
+
+public class ConsistecJobAd {
+
+	
+	public static JavaDeveloper findNewColleague() {
+		
+		// HasFun.workingWith(Beans.flavored(Jelly().or(Enterprise())));
+		
+		return null;
+	}
+}
---
+++ src/main/java/de/consistec/jobad/JavaDeveloper.java
@@ -0,0 +1,5 @@
+package de.consistec.jobad;
+
+public interface JavaDeveloper {
+
+}
