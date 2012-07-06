07ae34b0b0e97e9117910fd00a89c121690933a7
Christophe Boutter <boutta@gmail.com>
2012-07-06 16:13:51 +0200

    Finish add with attributes.

--- src/main/java/de/consistec/jobad/ConsistecJobAd.java
+++ src/main/java/de/consistec/jobad/ConsistecJobAd.java
@@ -1,32 +1,46 @@
 package de.consistec.jobad;
 
+import static de.consistec.jobad.Attribute.*;
+
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 public class ConsistecJobAd {
 	
-	public static JavaDeveloper findNewColleague() {
+	public static List<JavaDeveloper> findNewColleague() {
 		
-		List<Attribute> positiveAttributes = Arrays.asList();
-		List<Attribute> negativeAttributes = Arrays.asList();
+		List<Attribute> positiveAttributes = Arrays.asList(
+				ENTHUSIASTIC, LOVES_CLEAN_CODE, SOLUTION_DRIVEN, TEAMPLAYER, OPEN_MINDED);
+		
+		List<Attribute> negativeAttributes = Arrays.asList(JOB_HOPPER, LONER);
 		
 		List<JavaDeveloper> devs = 
 				findDeveloper(JavaDeveloper.class, Knows.JAVA, Knows.ENGLISH);
-		calculateDevScore(devs, positiveAttributes, negativeAttributes);
+		Map<JavaDeveloper, Integer> devScoreCard = 
+				calculateDevScore(devs, positiveAttributes, negativeAttributes);
 		
+		List<JavaDeveloper> newColleagues = Collections.<JavaDeveloper>emptyList();
+		for (Map.Entry<JavaDeveloper, Integer> entry : devScoreCard.entrySet()) {
+			
+			// Check applicant quality ;)
+			if (entry.getValue() > 3) {
+				newColleagues.add(entry.getKey());
+			}
+		}
 		
-		return null;
+		return newColleagues;
 		
-		// Find this code at: http://github.com/cboutter/jobad
+		// Find this (working) code at: http://github.com/cboutter/jobad
 	}
 	
 	
 	
-	private static void calculateDevScore(List<JavaDeveloper> devs,
+	private static <T extends Developer> Map<T, Integer> calculateDevScore(List<T> devs,
 			List<Attribute> positiveAttributes,
 			List<Attribute> negativeAttributes) {
-		// TODO Auto-generated method stub
-		
+		return null;
 	}
 
 	private static <T extends Developer> List<T> findDeveloper(Class<T> devClass, 
