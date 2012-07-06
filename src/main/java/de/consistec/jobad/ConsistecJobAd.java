4df8ef0eb00a12902200e4a76f4a7139ff6c48ea
Christophe Boutter <boutta@gmail.com>
2012-07-06 16:03:09 +0200

    Add idea of a dev score.

--- src/main/java/de/consistec/jobad/ConsistecJobAd.java
+++ src/main/java/de/consistec/jobad/ConsistecJobAd.java
@@ -1,21 +1,32 @@
 package de.consistec.jobad;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class ConsistecJobAd {
 	
 	public static JavaDeveloper findNewColleague() {
 		
+		List<Attribute> positiveAttributes = Arrays.asList();
+		List<Attribute> negativeAttributes = Arrays.asList();
 		
 		List<JavaDeveloper> devs = 
-				findDeveloper(JavaDeveloper.class, Knows.JAVA, Knows.ENGLISH, Knows.AGILE);
-		// Hibernate query with params to find a dev, filter further...
-		// HasFun.workingWith(Beans.flavored(Jelly().or(Enterprise())));
-		// groovy rubies...
+				findDeveloper(JavaDeveloper.class, Knows.JAVA, Knows.ENGLISH);
+		calculateDevScore(devs, positiveAttributes, negativeAttributes);
+		
 		
 		return null;
 		
 		// Find this code at: http://github.com/cboutter/jobad
+	}
+	
+	
+	
+	private static void calculateDevScore(List<JavaDeveloper> devs,
+			List<Attribute> positiveAttributes,
+			List<Attribute> negativeAttributes) {
+		// TODO Auto-generated method stub
+		
 	}
 
 	private static <T extends Developer> List<T> findDeveloper(Class<T> devClass, 
